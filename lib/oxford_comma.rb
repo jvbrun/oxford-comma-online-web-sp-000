9533281433b5cf239509b3d46c80724634acefee
Jean-Victor Brun <jeanvbrun@icloud.com>
2020-07-15 02:44:04 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
         array.join(" and ")
     elsif array.length > 2
         array.insert(-2, "and")
-        array[0..-1].join(", ") << array[-2..-1].join(" ")
+        array[0..-2].join(", ") << array[-2..-1].join(" ")
     else
         array.join
     end
