120affab67f24c13e95c24d38f15c191fcedaf88
Jean-Victor Brun <jeanvbrun@icloud.com>
2020-07-15 02:59:40 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,8 +2,8 @@ def oxford_comma(array)
   if array.length == 2
         array.join(" and ")
     elsif array.length > 2
-        array.insert(-2, "and")
-        array[0..-3].join(", ") << array[-1] = "and " + array[-1]
+ #       array.insert(-2, "and")
+        array[0..-2].join(", ") << array[-1] = "and " + array[-1]
     else
         array.join
     end
