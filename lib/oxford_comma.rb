603dcd28dc37e4c540ec7e064adcf76d9aa20412
Jean-Victor Brun <jeanvbrun@icloud.com>
2020-07-15 02:26:18 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
         array.join(" and ")
     elsif array.length > 2
         array.insert(-2, "and")
-        array[0..-2].join(", ")
+        array[0..-3].join(", ")
     else
         array.join
     end
